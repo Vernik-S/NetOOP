f0bbae9aa932bd2b19cc48fd964888f3d92de7c6
Vernik-S <vernik.krd@gmail.com>
2022-02-09 21:44:30 +0300

    Task 2 Attributes and interaction of classes

--- OOP.py
+++ OOP.py
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 class Student:
@@ -13,12 +13,24 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
+    def rate_lecturer(self, lecturer, course, grade): #Оценивать можно только завершенные курсы
+        if isinstance(lecturer, Lecturer) and course in self.finished_courses and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'    
+        
+        
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+class Reviewer(Mentor):
+        
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -27,30 +39,53 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
         
-class Reviewer(Mentor):
-    pass
-
 class Lecturer(Mentor):
-    pass
-        
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+# In[2]:
+
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+other_student = Student("William", "Gosset", "male")
+
+ 
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+ 
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+ 
+#print(best_student.grades)
+
+
+# In[3]:
+
+
+Gann = Lecturer("Gannibal", "Lector")
+Gann.courses_attached.append("Math")
+#print(Gann.courses_attached)
+best_student.finished_courses.append("Math")
+other_student.finished_courses.append("Math")
+
+
+# In[4]:
+
+
+best_student.rate_lecturer(Gann, "Math", 5)
+other_student.rate_lecturer(Gann, "Math", 10)
 
 
 # In[5]:
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+print(Gann.grades)
 
 
 # In[ ]:
